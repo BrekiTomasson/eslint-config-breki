42093faa5e465f6fdf498dfef2c7068f763ddb39
Breki Tomasson <breki.tomasson@gmail.com>
2018-04-01 14:24:10 +0200

    Updated ruleset, fixed minor bugs

--- test/option.test.js
+++ test/option.test.js
@@ -1,20 +1,24 @@
 const options = require('../options')
 
-describe("options", () => {
-  test("exports an object", () => {
-    expect(options).toBeDefined()
+describe('options test suite', () => {
+
+  test('exports an object', () => {
+    expect(options)
+      .toBeDefined()
   })
 
-  test("should have command named lint", () => {
-    expect(options.cmd).toBe("lint")
+  test('should have command named lint', () => {
+    expect(options.cmd)
+      .toBe('lint')
   })
 
-  test("should have cwd that is empty", () => {
-    expect(options.cwd).toBe("")
+  test('should have cwd that is empty', () => {
+    expect(options.cwd)
+      .toBe('')
   })
 
-  test("should have a bugs URL", () => {
-    expect(options.bugs).toBe("https://github.com/BrekiTomasson/breki-eslint-config/issues")
+  test('should have a bugs URL', () => {
+    expect(options.bugs)
+      .toBe('https://github.com/BrekiTomasson/breki-eslint-config/issues')
   })
-
 })
