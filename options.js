5bdb52972e52174c13b2d0d1299d099fb468229c
BrekiTomasson <breki.tomasson@gmail.com>
2018-12-06 15:42:39 +0100

    Removed all Vue-specific code.

--- options.js
+++ options.js
@@ -8,8 +8,8 @@ module.exports = {
   cwd: '',
   eslint: eslint,
   eslintConfig: {
-    configFile: path.join(__dirname, '.eslintrc'),
-    extensions: ['js', 'vue']
+    configFile: path.join(__dirname, '.eslintrc.json'),
+    extensions: ['js']
   },
   homepage: pkg.homepage,
   tagline: 'Honesty in small things is not a small thing.',
